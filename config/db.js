2cc57f5ab68b2eca413cc39e28c81ae65681dfce
deedaryani <daryanideepanshu@gmail.com>
2026-07-25 16:49:08 +0100

    changed code to host database on Tidb instead of Aiven

--- config/db.js
+++ config/db.js
@@ -10,7 +10,7 @@ const pool = mysql.createPool({
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-    ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: true } : undefined
+  ssl: process.env.DB_SSL === 'true' ? { minVersion: 'TLSv1.2', rejectUnauthorized: true } : undefined
 });
 
 module.exports = pool;
